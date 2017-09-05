4480b8d9470c950bfcfc370da2c67504c4451948
Samuel Silva <silva.samuel@gmail.com>
2017-09-05 22:38:15 +0100

    design new approach
    Signed-off-by: Samuel Silva <silva.samuel@gmail.com>

---
+++ Audit2Json.hs
@@ -0,0 +1,40 @@
+module Audit2Json where
+import Data.List
+import System.Directory (doesFileExist, getHomeDirectory)
+import System.FilePath.Windows (combine)
+
+version = "Egna-Audit2Json " ++ "version 2.0.0.0"
+
+getValue p k v = maybe v id (lookup k p)
+
+type Property = (String, String)
+getProperties = map (\x -> (read x) :: Property) . lines
+
+getPropertiesPath :: FilePath -> IO FilePath
+getPropertiesPath filename = do userDirectory <- getHomeDirectory
+                                existFilenameOnUser <- doesFileExist $ combine userDirectory filename
+                                return (if existFilenameOnUser then (combine userDirectory filename) else filename)
+
+
+isAuditTrash '\160' = True
+isAuditTrash '\NUL' = True
+isAuditTrash '\9632' = True
+isAuditTrash _ = False
+
+workflow props input = show
+                      $ map length
+                      $ lines 																-- cria listas com as linhas
+                      $ filter (not . isAuditTrash) input 									-- remove os carateres com lixo
+
+main :: IO ()
+main = do putStrLn version
+          propertiesPath <- getPropertiesPath "Egna-ReadAudit.prop"
+          putStrLn ("Read " ++ propertiesPath)
+          propsRaw <- readFile propertiesPath
+          props <- return $ getProperties propsRaw
+          putStrLn ("Read " ++ getValue props "input.filename" "DUMP.log")
+          input <- readFile $ getValue props "input.filename" "DUMP.log"
+          output <- return $ workflow props input
+          putStrLn output
+          return ()
+
